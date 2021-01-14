be51b3aaf838802d43bc448679bebee6d2b0d3c1
kirito20493 <62019415+kirito20493@users.noreply.github.com>
2021-01-15 06:37:06 +0700

    bt 1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// var screen = document.getElementById('display');
+// var btnNumber = document.getElementsByClassName('number');
+// // console.log(btnNumber.length);
+// for(var i=0;i<btnNumber.length;i++){
+//     switch (btnNumber[i].value){
+//         case 0:
+//         btnNumber[i].addEventListener('click',{
+//             screen
+//         })
+//     }
+    
+// }
