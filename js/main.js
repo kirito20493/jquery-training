16c71f483e1e8a611a1ccadbfdc7e5f5efa8821b
kirito20493 <62019415+kirito20493@users.noreply.github.com>
2021-01-15 06:44:46 +0700

    bt 4

--- js/main.js
+++ js/main.js
@@ -1,12 +1,77 @@
-// var screen = document.getElementById('display');
-// var btnNumber = document.getElementsByClassName('number');
-// // console.log(btnNumber.length);
-// for(var i=0;i<btnNumber.length;i++){
-//     switch (btnNumber[i].value){
-//         case 0:
-//         btnNumber[i].addEventListener('click',{
-//             screen
-//         })
-//     }
-    
-// }
+function Validator(options) {
+
+   var selectorRules = {};
+
+   function validate(inputElement, rule) {
+      var erroElement = inputElement.parentElement.querySelector(options.errorSelector);
+      var errorMessage;
+      var rules = selectorRules[rule.selector];
+      for (var i = 0; i < rules.length; ++i) {
+         errorMessage = rules[i](inputElement.value);
+         if (errorMessage) break;
+      }
+
+      if (errorMessage) {
+         erroElement.innerText = errorMessage;
+         inputElement.parentElement.classList.add('invalid');
+      } else {
+         erroElement.innerText = '';
+         inputElement.parentElement.classList.remove('invalid');
+      }
+   }
+
+
+   var formElement = document.querySelector(options.form);
+   if (formElement) {
+      options.rules.forEach(function (rule) {
+
+         if (Array.isArray(selectorRules[rule.selector])) {
+            selectorRules[rule.selector].push(rule.test);
+         } else {
+            selectorRules[rule.selector] = [rule.test];
+         }
+
+         var inputElement = formElement.querySelector(rule.selector);
+
+         if (inputElement) {
+
+            inputElement.onblur = function () {
+               validate(inputElement, rule);
+            }
+
+
+            inputElement.oninput = function () {
+               var erroElement = inputElement.parentElement.querySelector(options.errorSelector);
+               erroElement.innerText = '';
+               inputElement.parentElement.classList.remove('invalid');
+            }
+         }
+      })
+   }
+}
+
+Validator.isRequired = function (selector) {
+   return {
+      selector: selector,
+      test: function (value) {
+         return value.trim() ? undefined : 'Vui lòng nhập trường này!';
+      }
+   };
+}
+Validator.isEmail = function (selector) {
+   return {
+      selector: selector,
+      test: function (value) {
+         var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+         return regex.test(value) ? undefined : 'Email không hợp lệ!';
+      }
+   };
+}
+Validator.isNumber = function (selector) {
+   return {
+      selector: selector,
+      test: function (value) {
+         return isNaN(value) ? "Vui lòng chỉ nhập số!" : undefined;
+      }
+   };
+}
